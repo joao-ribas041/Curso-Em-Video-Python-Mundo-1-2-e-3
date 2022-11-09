eb27783b9a7ddbb729fde0171984de6e1dbef0c0
João Ribas <ribas.joao1410@gmail.com>
2022-11-08 23:40:45 -0300

    Update ex081.py

--- Exercicios/ex081.py
+++ Exercicios/ex081.py
@@ -3,4 +3,19 @@
 # B) A lista de valores ordenada de forma decrescente.
 # C) Se o valor 5 foi digitado e está ou não na lista.
 
+numeros = []
 
+while True:
+    numeros.append(int(input('Digite um valor: ')))
+    opc = str(input('Quer Continuar? [S/N] '))
+    if opc in 'Nn':
+        break
+print('-'*15)
+print(f'Você Digitou {len(numeros)} numeros.')
+numeros.sort()
+print(f'Valores ordenados: {numeros}')
+
+if 5 in numeros:
+    print('O valor 5 está na lista.')
+else:
+    print('O valor 5 não está na lista')
