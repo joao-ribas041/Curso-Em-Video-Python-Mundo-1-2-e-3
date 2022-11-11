9f73776fe032682ac3a29de207342ed296d2e319
João Ribas <ribas.joao1410@gmail.com>
2022-11-10 22:33:58 -0300

    Update ex083.py

--- Exercicios/ex083.py
+++ Exercicios/ex083.py
@@ -1,3 +1,19 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com
 # os parênteses aberto e fechados na ordem correta.
+expressao = str(input('Digite a expressao para teste: '))
+lista = []
+for caracter in expressao:
+    if caracter == '(':
+        lista.append('(')
+    elif caracter == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+
+if len(lista) == 0:
+    print('Expressao correta.')
+else:
+    print('Expressao errada.')
