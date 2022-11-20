3fe0026bcbf8b60f5cb4e4c4ab717e3759489279
João Ribas <ribas.joao1410@gmail.com>
2022-11-19 23:57:39 -0300

    Update ex095.py

--- Exercicios/ex095.py
+++ Exercicios/ex095.py
@@ -1,15 +1,18 @@
 # Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo
 # um sistema de visualização de detalhes do aproveitamento de cada jogador.
 jogadores = []
-
 jogador = {}
 gols = []
+maior_participacao = 0
 
 while True:
     gols_totais = 0
     jogador['nome'] = str(input('Digite o nome do jogador: ')).capitalize()
     jogador['partidas'] = int(
         input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    if maior_participacao < jogador['partidas']:
+        maior_participacao = jogador['partidas']
+
     for i in range(0, jogador['partidas']):
         gol = int(
             input(f'Quantos gols {jogador["nome"]} fez no {i+1}o. jogo: '))
@@ -26,9 +29,19 @@ while True:
     print()
     if continua in 'Nn':
         break
-print('-'*30)
-print(f'{"cod":^3} {"nome":<8} {"Partidas":<3} {"gols":<5} {"Total":<3}')
-print('-'*30)
+
+print(maior_participacao)
+print('-'*40)
+print(f'{"cod":^3} {"nome":<8} {"Partidas":<8} {"gols"} {"Total":>7}')
+print('-'*40)
 for i, j in enumerate(jogadores):
     print(
-        f'{i+1:^3} {j["nome"]:<8} {j["partidas"]:<3} {j["gols-por-partida"]:<5} {j["gols-totais"]:<3}')
+        f'{i:^3} {j["nome"]:<8} {j["partidas"]:<8} {j["gols-por-partida"]} {j["gols-totais"]:>7}')
+print('-'*40)
+
+while True:
+    consulta = int(input('Deseja mostrar dados de qual jogador? '))
+    if 0 > consulta > len(jogadores):
+        print(f'Não existe o codigo {consulta}.')
+    else:
+        print(f'-- Levantamento do jogador {jogadores[consulta]["nome"]}')
