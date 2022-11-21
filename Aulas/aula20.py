5fd62a62bc3af7401754d7bd28201977b18cf983
João Ribas <ribas.joao1410@gmail.com>
2022-11-21 20:51:38 -0300

    Update aula20.py

--- Aulas/aula20.py
+++ Aulas/aula20.py
@@ -1,4 +1,4 @@
-def linha():
+""" def linha():
     print('-' * 30)
 
 
@@ -10,4 +10,24 @@ def titulo(msg):
 
 titulo('CURSO EM VIDEO')
 titulo('APRENDA PYTHON')
-titulo('GUSTAVO GUANABARA')
+titulo('GUSTAVO GUANABARA') """
+
+
+""" def soma(a, b):
+    s = a + b
+    print(s)
+
+
+soma(4, 5)
+soma(8, 9)
+soma(2, 1) """
+
+
+def soma(*valores):
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}')
+
+
+soma(5, 2)
+soma(2, 9, 4)
