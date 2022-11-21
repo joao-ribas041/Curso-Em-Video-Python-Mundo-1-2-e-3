13f9303268349221db7aae12990af8df5f109bf2
João Ribas <ribas.joao1410@gmail.com>
2022-11-21 20:36:15 -0300

    Create aula20.py

---
+++ Aulas/aula20.py
@@ -0,0 +1,13 @@
+def linha():
+    print('-' * 30)
+
+
+def titulo(msg):
+    print('-' * 30)
+    print(f'{msg:^30}')
+    print('-' * 30)
+
+
+titulo('CURSO EM VIDEO')
+titulo('APRENDA PYTHON')
+titulo('GUSTAVO GUANABARA')
