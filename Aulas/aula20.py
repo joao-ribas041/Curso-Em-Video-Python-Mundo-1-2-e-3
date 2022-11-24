db10e718313ab2ffa36a7941b5de8d7bf0caa663
João Ribas <ribas.joao1410@gmail.com>
2022-11-23 22:04:58 -0300

    aula 20

--- Aulas/aula20.py
+++ Aulas/aula20.py
@@ -23,11 +23,25 @@ soma(8, 9)
 soma(2, 1) """
 
 
-def soma(*valores):
+""" def soma(*valores):
     for num in valores:
         s += num
     print(f'Somando os valores {valores} temos {s}')
 
 
 soma(5, 2)
-soma(2, 9, 4)
+soma(2, 9, 4) """
+
+
+def dobra(lst):
+    pos = 0
+    while pos < len(lst):
+        print(f'Valor {valores[pos]} dobrado: {lst[pos] * 2}')
+        lst[pos] *= 2
+        pos += 1
+
+
+valores = [6, 3, 9, 1, 0, 2]
+
+dobra(valores)
+print(valores)
