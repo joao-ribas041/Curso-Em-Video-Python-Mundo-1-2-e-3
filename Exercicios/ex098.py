60f807e4735b85448df5afd709db6b9422b9c2d5
João Ribas <ribas.joao1410@gmail.com>
2022-11-24 23:30:51 -0300

    Update ex098.py

---
+++ Exercicios/ex098.py
@@ -0,0 +1,16 @@
+# Faça um programa que tenha um função chamada contador(), que receba
+# três parâmetros: início, fim e passo e realize a contagem.
+# Seu programa tem que realizar três contagens através da função criada:
+# a)De 1 até 10, de 1 em 1
+# b)De 10 até 0, de 2 em 2
+# c)Uma contagem personalizada.
+
+def contador(inicio, fim, passo):
+
+    for i in range(inicio, fim+1, passo):
+        print(i, end=' ')
+    print()
+
+
+contador(1, 10, 1)
+contador(10, -1, -2)
