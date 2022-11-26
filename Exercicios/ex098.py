8d0cf09232ea37807be6dc4554133c4a9e9cd0bc
João Ribas <ribas.joao1410@gmail.com>
2022-11-25 21:32:04 -0300

    ex098

--- Exercicios/ex098.py
+++ Exercicios/ex098.py
@@ -5,12 +5,39 @@
 # b)De 10 até 0, de 2 em 2
 # c)Uma contagem personalizada.
 
-def contador(inicio, fim, passo):
+from time import sleep
+linha = '~'*40
 
-    for i in range(inicio, fim+1, passo):
-        print(i, end=' ')
-    print()
+
+def contador(inicio, fim, passo):
+    print(linha)
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+    sleep(0.5)
+
+    if passo <= 0:
+        passo = 1
+
+    if fim > inicio:
+        fim += 1
+        for i in range(inicio, fim, passo):
+            print(i, end=' ')
+        print()
+
+    # elif inicio > fim:
+    else:
+        fim -= 1
+        passo *= -1
+        for i in range(inicio, fim, passo):
+            print(i, end=' ')
+        print()
 
 
 contador(1, 10, 1)
-contador(10, -1, -2)
+contador(10, 0, 2)
+print(linha)
+print('Sua Vez.')
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contador(i, f, p)
+print('Fim')
