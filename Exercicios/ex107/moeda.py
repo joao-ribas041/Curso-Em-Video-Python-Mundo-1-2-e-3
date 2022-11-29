957c87e9ca31edaad0badee4f5cd7e6ecd37d597
João Ribas <ribas.joao1410@gmail.com>
2022-11-28 23:33:31 -0300

    ex107

--- Exercicios/ex107/moeda.py
+++ Exercicios/ex107/moeda.py
@@ -1,16 +1,18 @@
 def aumentar(valor=0, porcentagem=0):
-    valor += (valor * porcentagem) / 100
-    return valor
+    resultado = valor + (valor * porcentagem / 100)
+    return resultado
 
 
 def diminuir(valor=0, porcentagem=0):
-    valor -= (valor * porcentagem) / 100
-    return valor
+    resultado = valor - (valor * porcentagem / 100)
+    return resultado
 
 
 def dobro(valor=0):
-    return valor * 2
+    resultado = valor * 2
+    return resultado
 
 
 def metade(valor=0):
-    return valor / 2
+    resultado = valor / 2
+    return resultado
